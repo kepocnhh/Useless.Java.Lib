0f4b08942b764b32fd95a1f025cac4df156fb28f
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-02 17:02:13 +0300

    init lib

---
+++ lib/build.gradle.kts
@@ -0,0 +1,105 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import sp.gx.core.Badge
+import sp.gx.core.GitHub
+import sp.gx.core.Markdown
+import sp.gx.core.Maven
+import sp.gx.core.asFile
+import sp.gx.core.assemble
+import sp.gx.core.buildDir
+import sp.gx.core.check
+import sp.gx.core.create
+import sp.gx.core.task
+
+version = "0.0.1"
+
+val maven = Maven.Artifact(
+    group = "com.github.kepocnhh",
+    id = rootProject.name,
+)
+
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
+
+repositories.mavenCentral()
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
+val compileKotlinTask = tasks.getByName<KotlinCompile>("compileKotlin") {
+    kotlinOptions {
+        jvmTarget = Version.jvmTarget
+        freeCompilerArgs += setOf("-module-name", maven.moduleName())
+    }
+}
+
+"unstable".also { variant ->
+    val version = "${version}u-SNAPSHOT"
+    tasks.create("check", variant, "Readme") {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven, version)),
+                "implementation(\"${maven.moduleName(version)}\")",
+            )
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = buildDir()
+                    .dir("reports/analysis/readme")
+                    .asFile("index.html"),
+            )
+        }
+    }
+    tasks.create("assemble", variant, "MavenMetadata") {
+        doLast {
+            val file = buildDir()
+                .dir("yml")
+                .file("maven-metadata.yml")
+                .assemble(
+                    """
+                        repository:
+                         groupId: '${maven.group}'
+                         artifactId: '${maven.id}'
+                        version: '$version'
+                    """.trimIndent(),
+                )
+            println("Metadata: ${file.absolutePath}")
+        }
+    }
+    task<Jar>("assemble", variant, "Jar") {
+        dependsOn(compileKotlinTask)
+        archiveBaseName = maven.id
+        archiveVersion = version
+        from(compileKotlinTask.destinationDirectory.asFileTree)
+    }
+    task<Jar>("assemble", variant, "Source") {
+        archiveBaseName = maven.id
+        archiveVersion = version
+        archiveClassifier = "sources"
+        from(sourceSets.main.get().allSource)
+    }
+    tasks.create("assemble", variant, "Pom") {
+        doLast {
+            val file = buildDir()
+                .dir("libs")
+                .file("${maven.name(version)}.pom")
+                .assemble(
+                    maven.pom(
+                        version = version,
+                        packaging = "jar",
+                    ),
+                )
+            println("POM: ${file.absolutePath}")
+        }
+    }
+}
